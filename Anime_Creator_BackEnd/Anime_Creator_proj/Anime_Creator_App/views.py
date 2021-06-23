ef740224518fb275ab22056e25228b44f0d38afb
DAVID paredes <augustspies22@gmail.com>
2021-06-23 09:55:03 -0700

    Comment View created

--- Anime_Creator_BackEnd/Anime_Creator_proj/Anime_Creator_App/views.py
+++ Anime_Creator_BackEnd/Anime_Creator_proj/Anime_Creator_App/views.py
@@ -4,6 +4,8 @@ from .models import AnimeCreator, Comment, Rating
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializer import CommentSerializer, RatingSerializer, AnimeCreatorSerializer
+from django.http import Http404
 
 # Create your views here.
 
@@ -16,13 +18,31 @@ def index(request):
     return render(request, 'Anime_Creator_App/index.html', context)
 
 
-class CommentList(APIView):
+class CommentView(APIView):
 
     def get(self, request):
-        if request.method == 'GET':
-            comments = Comment.objects.all()
-            currentComments = request.query_params.get('currentComments', None)
-            if currentComments is not None:
-                comments = comments.filter(currentComments_icontains=currentComments)
-            serializer = CommentSerializer(comments, many=True)
-            return Response(serializer.data)
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_object(self, pk):
+        try:
+            return Comment.objects.get(pk=pk)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def delete(self, request, comment_id):
+        comment = self.get_object(pk=comment_id)
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.delete()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_204_NO_CONTENT)
+
